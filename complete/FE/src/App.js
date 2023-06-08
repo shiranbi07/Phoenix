36c4f335bbd16bf931eb4132127773341756c388
Tzachi Halevy <tzachi81@users.noreply.github.com>
2023-06-08 10:20:15 +0300

    style spinner loader

--- complete/FE/src/App.js
+++ complete/FE/src/App.js
@@ -1,39 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Container, Icon, Header, Form, Segment } from 'semantic-ui-react'
 import SuccessMessage from './components/messages/SuccessMessage';
 import ErrorMessage from './components/messages/ErrorMessage';
+import Loader from './components/loader/loader';
 
 const App = () => {
     const [filePath, setFilePath] = useState('');
     const [results, setresults] = useState('');
-    // const apiUrl = 'http: //10.201.191.227 :8080/checkCode';
+    const [isLoading, setIsLoading] = useState(false);
 
     const apiUrl = 'http://10.189.150.30:8080/checkCode';
-    // const apiUrl = 'localhost:8080/checkCode';
-    const data =  {filePath} ;
+    const data = { filePath };
 
-    // {"filePath" : "https://github.com/shiranbi07/Phoenix/blob/main/hackaton/amat/java/bug/BankAccount.java"}
     const apiOptions = {
         method: "POST",
-        body: 
+        body:
             // "filePath" : "https://github.com/shiranbi07/Phoenix/blob/main/hackaton/amat/java/bug/BankAccount.java"
             // '{"filePath" : "https://github.com/shiranbi07/Phoenix/blob/main/python_examples/concatenate_numbers.py"}'
             JSON.stringify(data)
         ,
         headers: {
             "Content-Type": "application/json"
-          },
+        },
     }
     const onSetUpdatefile = (e) => {
         setFilePath(e.currentTarget.value);
     }
-    
+
     const onCodeCheckclick = async () => {
+        
+        setIsLoading(true);
+
         const res = await fetch(apiUrl, apiOptions)
             .then((response => response.json()))
             .then(data => {
+                setIsLoading(false);
                 setresults(data);
-                return data
             })
     }
 
@@ -43,10 +45,11 @@ const App = () => {
 
     return (
         <div>
+            {isLoading && <Loader />}
             <Container>
                 <Header textAlign='center' className='ui primary header' as='h1'>
                     <Icon.Group size='big'>
-                        <Icon name='bug' />
+                        <Icon name='bug' color='blue'/>
                     </Icon.Group>
 
                     AI Code Linter
@@ -58,11 +61,11 @@ const App = () => {
                             value={filePath} onChange={onSetUpdatefile}
                         />
                         <Form.Group inline>
-                            <Form.Button primary
+                            <Form.Button primary 
                                 onClick={onCodeCheckclick}>
                                 Check Code
                             </Form.Button>
-                            <Form.Button color='grey' onClick={onClearText}>
+                            <Form.Button color='lightgrey' onClick={onClearText}>
                                 Clear Text
                             </Form.Button>
                         </Form.Group>
@@ -71,7 +74,7 @@ const App = () => {
                     {(Object.keys(results).length > 0) && (
                         <>
                             <div>
-                                
+
                                 {(results?.errorNumbers === 0) ? <SuccessMessage /> : <ErrorMessage errors={results.errorNumbers} />}
 
                                 <div className="ui horizontal segments">
