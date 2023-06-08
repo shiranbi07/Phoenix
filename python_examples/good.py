0531b6c15220f37da566370c2fea6294bedfbdd7
adiArdenis1981 <135692113+adiArdenis1981@users.noreply.github.com>
2023-06-08 10:58:17 +0300

    Add files via upload

---
+++ python_examples/good.py
@@ -0,0 +1,15 @@
+# This function takes two numbers from the user, adds them together and returns their sum
+
+def add_numbers(a, b):
+
+result = a + b
+
+return result
+
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+sum = add_numbers(num1, num2)
+
+print("The sum is: " + str(sum))
